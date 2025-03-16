90d630ee4e53a428e593c7532606a5b4b6ee6481
Igor Viana <igorvianacont@gmail.com>
2025-03-16 13:27:54 -0300

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  // template:`<div class="red-text">Hello World Angular {{name}} is here</div>`
 })
 export class AppComponent {
   title = 'first-steps-angular';
+  name = "Igor Viana"
 }
